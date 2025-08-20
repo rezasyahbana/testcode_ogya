a6dc7600fbd95afc93a9ce0ef7d78d75020fa5be
rezasyahbana <123437760+rezasyahbana@users.noreply.github.com>
2025-08-20 16:01:03 +0700

    Delete 1.voltage-generic-save-point-v2-(SERVICE GENERIC)/voltage-generic-save-point-2 directory

--- 1.voltage-generic-save-point-v2-(SERVICE GENERIC)/voltage-generic-save-point-2/src/main/java/com/volt/gen/service/KafkaProducerService.java
+++
@@ -1,8 +0,0 @@
-package com.volt.gen.service;
-
-import com.telkom.logging.avro.schema.LogObject;
-
-public interface KafkaProducerService {
-    void send(LogObject logObject);
-    void sendRaw(String rawMessage);
-}
--- 1.voltage-generic-save-point-v2-(SERVICE GENERIC)/voltage-generic-save-point-2/src/main/java/com/volt/gen/service/VoltageFpeService.java
+++
@@ -1,7 +0,0 @@
-package com.volt.gen.service;
-
-public interface VoltageFpeService {
-
-    String transformData(String transformId, String body);
-    String checkService(String data);
-}
