ddadef60c595c1bca50622fa045e2da453cca44c
rezasyahbana <123437760+rezasyahbana@users.noreply.github.com>
2026-01-06 15:23:56 +0700

    Delete telkom-config-sda-service directory

--- telkom-config-sda-service/src/main/java/org/tk/sda/config/util/DuplicateIdException.java
+++
@@ -1,7 +0,0 @@
-package org.tk.sda.config.util;
-
-public class DuplicateIdException extends RuntimeException {
-    public DuplicateIdException(String message) {
-        super(message);
-    }
-}
--- telkom-config-sda-service/src/main/java/org/tk/sda/config/util/IdDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package org.tk.sda.config.util;
-
-public class IdDoesNotExistException extends RuntimeException{
-    public IdDoesNotExistException(String message) {
-        super(message);
-    }
-}
